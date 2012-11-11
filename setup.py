70377f6b3134a4093913048ec869976753c1f635
Jamie Paton <jamie.paton@googlemail.com>
2012-11-10 20:48:39 -0800

    Fix an issue with README linking in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+import os
 from distutils.core import setup
+
+this_path = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name='craft',
-    version='0.2.0',
+    version='0.2.4',
     author='Jamie Paton',
     author_email='jamie.paton@googlemail.com',
     packages=['craft'],
@@ -10,7 +13,7 @@ setup(
     url='http://pypi.python.org/pypi/craft/',
     license='LICENSE.txt',
     description='Craft - Like Makefile, but in Python.',
-    long_description=open('README.pypi').read(),
+    long_description=open(os.path.join(this_path,'README.pypi')).read(),
     install_requires=[
         "termcolor == 1.1.0",
     ],
